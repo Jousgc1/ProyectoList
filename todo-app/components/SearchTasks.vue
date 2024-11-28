9a66b8cecf000db268796982cff00a922218777d
[Alejandro Jose Garcia Chaname] <[chana_15@outlook.com]>
2024-11-28 04:16:28 -0500

    finalizando y comentando

--- todo-app/components/SearchTasks.vue
+++ todo-app/components/SearchTasks.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
+    <!-- Campo de entrada para buscar tareas -->
     <input v-model="title" placeholder="Buscar tareas" @input="fetchTasks" />
+    <!-- Lista de tareas renderizada dinámicamente -->
     <ul>
       <li v-for="task in tasks" :key="task.id">{{ task.title }}: {{ task.description }}</li>
     </ul>
@@ -13,14 +15,18 @@ import gql from 'graphql-tag';
 export default {
   data() {
     return {
+      // Almacena el título para buscar tareas
       title: '',
+      // Almacena las tareas obtenidas de la consulta GraphQL
       tasks: [],
     };
   },
   methods: {
+    // Método para buscar tareas utilizando Apollo Client
     async fetchTasks() {
       const client = this.$apollo.provider.defaultClient;
       const { data } = await client.query({
+        // Definición de la consulta GraphQL para obtener tareas
         query: gql`
           query GetTasks($title: String) {
             getTasks(title: $title) {
@@ -30,8 +36,10 @@ export default {
             }
           }
         `,
+        // Pasa el título como variable a la consulta
         variables: { title: this.title },
       });
+      // Actualiza la lista de tareas con los resultados de la consulta
       this.tasks = data.getTasks;
     },
   },
